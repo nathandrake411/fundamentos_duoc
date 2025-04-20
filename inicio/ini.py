876c227941af76d1a7bafc6d33afa8551626911b
axel <natandreik11@gmail.com>
2025-04-19 22:27:46 -0400

    agrege texto

--- inicio/ini.py
+++ inicio/ini.py
@@ -1,8 +1,8 @@
 print("axel")
 print("18")
-
 print("faint")
 print(146 + 134)
 print("probando imprimir texto en python")
 print("probando guardarlo")
 print("no consigo guardarlo")
+print("hola holaaaa")
