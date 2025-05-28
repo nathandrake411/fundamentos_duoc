ff36ddde3dd6c60415817f0f3e590cd4cdcd20b5
axel <natandreik11@gmail.com>
2025-05-28 11:18:26 -0400

    nuevo text

---
+++ inicio/regalito.py
@@ -0,0 +1,12 @@
+numero = int(input("ingrese un numero:"))
+print("el numero es,")
+
+num = numero == float
+
+if num is float:
+    print("epa")
+    if numero // 1:
+        print("es primo")
+    else:
+        print("no es primo")
+
