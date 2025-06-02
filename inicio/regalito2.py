94af3d9247452c60be62b792ca6d2ff172e193c4
axel <natandreik11@gmail.com>
2025-06-02 14:52:11 -0400

    new code

--- inicio/regalito2.py
+++ inicio/regalito2.py
@@ -1,9 +1,25 @@
-numero = int(input("ingrese un numero entre 0 y 100:"))
+ingresar_numero = False
+salir = False
+print("1-ingresar numero")
+print("2-mostrar mayor")
+print("3-mostrar menor")
+print("4-salir")
 
-while numero > 100:
-    print("debe ser dentro del rango")
-if numero == range(0, 100):
-    print("bien")
+opcion_usuario = int(input("ingrese su opcion:"))
 
-    
+
+while opcion_usuario == 1:
+    numero = []
+    numero = int(input("ingrese un numero entero entre 0 y 100:"))
+    numero.append(numero)
+    if 0<numero>100:
+       print("debes ingresar un numero dentro de lo establecido")
+    else:
+        print("ingreso exitoso")
+
+if opcion_usuario == 2:
+     print("su numero mayor ingresado es ")
+
+
+
 
