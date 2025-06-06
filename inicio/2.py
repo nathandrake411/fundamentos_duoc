6f7cee6f2ab51dca32ceb755ec156c3e0b478ae4
axel <natandreik11@gmail.com>
2025-06-05 20:11:46 -0400

    new

---
+++ inicio/2.py
@@ -0,0 +1,7 @@
+peso_kilogramos = int(input("cual es tu peso en kilogramos:"))
+altura_metros = float(input("cual es tu altura en metros:"))
+operacion = (peso_kilogramos / altura_metros)
+
+print("su peso es", peso_kilogramos)
+print("su altura es", altura_metros)
+print("su mic es", operacion)
--- inicio/ini.py
+++ inicio/ini.py
@@ -1,6 +1,9 @@
-print("axel")
-print("18")
-print("faint")
-print(146 + 134)
-print("probando imprimir texto en python")
-print("probando guardarlo")
+nombre = input("cual es tu nombre:")
+edad = int(input("cual es tu edad:"))
+ciudad = input("cual es tu ciudad:")
+estudiante = True
+
+print("hola me llamo", nombre)
+print("mi edad es", edad)
+print("vivo en", ciudad)
+print("soy estudiante:",estudiante)
